be83ae3a73ed6ed75f3f55f0203ae53600057bef
Michael Babker <mbabker@flbab.com>
2011-07-25 09:50:07 +0430

    Seed repo

---
+++ mod_github.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* Tweet Display Back Module for Joomla!
+*
+* @copyright	Copyright (C) 2010-2011 Michael Babker. All rights reserved.
+* @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+*/
+
+// No direct access
+defined('_JEXEC') or die;
+
+// Check if cURL is loaded; if not, proceed no further
+if (!extension_loaded('curl')) {
+	echo JText::_('MOD_GITHUB_ERROR_NOCURL');
+	return;
+}
+
+// Include the helper
+require_once dirname(__FILE__).'/helper.php';
+
+// Check if caching is enabled
+if ($params->get('cache') == 1) {
+	// Set the cache parameters
+	$options = array(
+		'defaultgroup' => 'mod_github');
+	$cache		= JCache::getInstance('callback', $options);
+	$cacheTime	= $params->get('cache_time');
+	// J! 1.5 and 1.6 cache is set in seconds, 1.7 caches in minutes
+	if (version_compare(JVERSION,'1.7.0','ge')) {
+		$cacheTime	= round($cacheTime / 60);
+	}
+	$cache->setLifeTime($cacheTime);
+	$cache->setCaching(true);
+
+	// Call the cache; if expired, pull new data
+	$twitter = $cache->call(array('modGithubHelper', 'compileData'), $params);
+} else {
+	// Pull new data
+	$twitter = modGithubHelper::compileData($params);
+}
+
+if (isset($twitter->hits)) {
+	echo JText::_('MOD_GITHUB_ERROR_NOHITS');
+	return;
+} else if ((!$twitter) || (isset($twitter->error))) {
+	echo JText::_('MOD_GITHUB_ERROR_UNABLETOLOAD');
+	return;
+}
+
+require(JModuleHelper::getLayoutPath('mod_github', $params->get('templateLayout', 'default')));
